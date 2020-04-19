e440eda4294efe9ba34a2f1681d6d58d17163434
rotunstall <rotunstall@gmail.com>
2020-04-18 21:25:02 -0400

    working on edit task feature

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Define UI Vars
+// DEFINE UI Vars
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
@@ -7,31 +7,84 @@ const taskInput = document.querySelector('#task');
 const editItem = document.querySelector('.edit-item');
 const completed = document.querySelector('.completed');
 
-// Load all event listeners
+// LOAD all event listeners
 loadEventListeners();
 
-// Load all event listeners
+// LOAD all event listeners
 function loadEventListeners() {
     // DOM load event
     document.addEventListener('DOMContentLoaded', getTasks);
-    // Add task event
+    // ADD task event
     form.addEventListener('submit', addTask);
-    // Remove task event    removeTask
-    taskList.addEventListener('click', taskItemClicked);
-    // Clear task event  
+    // All click events on the li
+    taskList.addEventListener('click', whenTaskItemLiIsClicked);
+    // CLEAR task event  
     clearBtn.addEventListener('click', clearTasks);
-    // Filter task event
+    // FILTER task event
     filter.addEventListener('keyup', filterTasks);
-    // Edit task event
-    // editItem.addEventListener('click', editTask);
+
 }
 
 
-function taskItemClicked(e) {
-    // const target = e.target;
+function whenTaskItemLiIsClicked(e) {
+    //const target = e.target;
     //delete --- edit-- - completed
-    // console.log(e);
 
+
+    if (e.target.parentElement.classList.contains('edit-item')) {
+        // MAKE task-text editable and focused immediately
+        const currentText = e.target.parentElement.previousElementSibling;
+        currentText.contentEditable = true;
+        e.target.parentElement.previousElementSibling.focus();
+
+
+
+
+
+        // CHANGE the "EDIT" and "DELETE" buttons to "SAVE" and "CANCEL" buttons respectively
+        const currentLi = e.target.parentElement.parentElement;
+        //const currentEditA = e.target.parentElement;
+        //const currentDeleteA = e.target.parentElement.nextElementSibling;
+        const updateBtn = document.createElement('a');
+        updateBtn.className = 'btn-floating btn-small waves-effect waves-light  update-btn';
+        updateBtn.innerHTML = '<i class="material-icons">add</i>';
+        //currentLi.replaceChild(updateBtn, currentEditA);
+        currentLi.lastChild.style.display = "none";
+        currentLi.lastChild.previousElementSibling.style.display = "none";
+        currentLi.appendChild(updateBtn);
+
+        const cancelBtn = document.createElement('a');
+        cancelBtn.className = 'btn-floating btn-small waves-effect waves-light cancel-btn';
+        cancelBtn.innerHTML = '<i class="material-icons">remove</i>';
+        //currentLi.replaceChild(cancelBtn, currentDeleteA);
+        currentLi.appendChild(cancelBtn);
+    }
+
+    // STORE the original text in a variable
+    const originalText = (((e.target.parentElement.previousElementSibling.classList.contains('task-text')) && (e.target.parentElement.previousElementSibling.contentEditable === "true")) || (e.target.parentElement.classList.contains('cancel-btn'))) ? e.target.parentElement.previousElementSibling.textContent : '';
+
+
+    // UNDO changes when the "CANCEL button" is clicked
+    if (e.target.parentElement.classList.contains('cancel-btn')) {
+        createTaskListItem(originalText);
+
+    }
+
+    // if (e.target.parentElement.classList.contains('edit-item')) {
+    //     e.target.parentElement.previousElementSibling.focus();
+
+    // }
+    // disable all clicking except currentsave, currentcancel, currenttask-text until currentsave or currentcancel has been clicked
+
+
+    //let originalText = e.target.textContent;
+    console.log(originalText);
+    // if ((e.target.classList.contains('task-text')) && (e.target.contentEditable === "true")) {
+
+
+    // }
+
+    // REMOVE list item when "delete" icon is clicked
     if (e.target.parentElement.classList.contains('delete-item')) {
         if (confirm('Are you sure you want to delete this task?')) {
             e.target.parentElement.parentElement.remove();
@@ -39,21 +92,24 @@ function taskItemClicked(e) {
             // Remove from Local Storage
             removeTaskFromLocalStorage(e.target.parentElement.parentElement.firstElementChild.nextElementSibling);
         }
-    } else if (e.target.parentElement.classList.contains('edit-item')) {
+        // } else if (e.target.parentElement.classList.contains('edit-item')) {
+        //     const currentText = e.target.parentElement.previousElementSibling;
+        //     currentText.contentEditable = true;
+        //     const currentLi = e.target.parentElement.parentElement;
+        //     const currentEditA = e.target.parentElement;
+        //     const currentDeleteA = e.target.parentElement.nextElementSibling;
 
-        const currentText = e.target.parentElement.previousElementSibling;
-        currentText.contentEditable = true;
-        const currentLi = e.target.parentElement.parentElement;
-        const currentEditA = e.target.parentElement;
-        const currentDeleteA = e.target.parentElement.nextElementSibling;
-        console.log(currentText);
-        currentText.addEventListener('click', function(e) {
-            const updateBtn = document.createElement('a');
-            updateBtn.className = 'waves-effect waves-light btn update-btn';
-            updateBtn.textContent = 'UPDATE';
-            console.log(updateBtn);
-            currentLi.replaceChild(updateBtn, currentEditA);
-        });
+        //     currentText.addEventListener('click', function(e) {
+        //         const updateBtn = document.createElement('a');
+        //         updateBtn.className = 'btn-floating btn-small waves-effect waves-light  update-btn';
+        //         updateBtn.innerHTML = '<i class="material-icons">add</i>';
+        //         currentLi.replaceChild(updateBtn, currentEditA);
+
+        //         const cancelBtn = document.createElement('a');
+        //         cancelBtn.className = 'btn-floating btn-small waves-effect waves-light cancel-btn';
+        //         cancelBtn.innerHTML = '<i class="material-icons">remove</i>';
+        //         currentLi.replaceChild(cancelBtn, currentDeleteA);
+        //     });
         // change icons to "save" and "cancel"
     } else if ((e.target.classList.contains('completed')) && (e.target.checked === true)) {
         // Add checked to item in Local Storage
@@ -66,7 +122,7 @@ function taskItemClicked(e) {
 
 
 function createTaskListItem(textSource) {
-    // Create li element
+    // CREATE li element, label element, span element, input:checkbox element, and Append to li
     const li = document.createElement('li');
     li.className = 'collection-item';
 
@@ -76,34 +132,33 @@ function createTaskListItem(textSource) {
     const span = document.createElement('span');
     span.className = 'checkbox-helper';
 
-    // Create new check box element and append to li
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    checkBox.className = 'completed filled-in';
+    const inputCheck = document.createElement('input');
+    inputCheck.type = 'checkbox';
+    inputCheck.className = 'completed filled-in';
 
-    label.appendChild(checkBox);
+    label.appendChild(inputCheck);
     label.appendChild(span);
     li.appendChild(label);
 
-    // Create text node in a span element and append to label
+    // CREATE text node in a paragraph element with text content and append to label
     const para = document.createElement('p');
     para.className = 'task-text';
     para.appendChild(document.createTextNode(textSource));
     li.appendChild(para);
 
-    // Create new edit link element
+    // CREATE  "edit" link element
     const editLink = document.createElement('a');
     editLink.className = 'edit-item';
     editLink.innerHTML = '<i class="small material-icons">edit</i>';
     li.appendChild(editLink);
 
-    // Create new link element
+    // CREATE "delete" link element
     const link = document.createElement('a');
     link.className = 'delete-item';
     link.innerHTML = '<i class="small material-icons">delete</i>';
     li.appendChild(link);
 
-    // Append the li to ul
+    // APPEND the li to ul
     taskList.appendChild(li);
 
 
@@ -112,7 +167,7 @@ function createTaskListItem(textSource) {
 
 
 
-// Get Task from Local Storage
+// GET task from Local Storage(function) and CREATE task in the UI(called)
 function getTasks() {
     let tasks;
     if (localStorage.getItem('tasks') === null) {
@@ -126,7 +181,7 @@ function getTasks() {
     });
 }
 
-//Add Task
+// VALIDATE entry, CREATE task in the UI(called) and STORE task in Local Storage(called)
 function addTask(e) {
     if (taskInput.value === '') {
         alert('Add a task, please.');
@@ -139,20 +194,7 @@ function addTask(e) {
     }
     e.preventDefault();
 }
-
-//Edit Task
-function editTask() {
-    // let li = e.target.parentElement;
-    //console.log(editItem);
-
-    //  .contentEditable = true;
-    const completed = document.querySelector('.completed');
-    completed.setAttribute("checked", "checked");
-
-    // e.preventDefault();
-}
-
-// Store Task
+// STORE task to Local Storage(function)
 function storeTaskInLocalStorage(task) {
     let tasks;
     if (localStorage.getItem('tasks') === null) {
@@ -165,19 +207,8 @@ function storeTaskInLocalStorage(task) {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-// Remove Task
-function removeTask(e) {
-    if (e.target.parentElement.classList.contains('delete-item')) {
-        if (confirm('Are you sure you want to delete this task?')) {
-            e.target.parentElement.parentElement.remove();
 
-            // Remove from Local Storage
-            removeTaskFromLocalStorage(e.target.parentElement.parentElement.firstElementChild.firstElementChild.nextElementSibling);
-        }
-    }
-}
-
-// Remove from localStorage
+// REMOVE task from Local Storage(function)
 function removeTaskFromLocalStorage(taskItem) {
     let tasks;
     if (localStorage.getItem('tasks') === null) {
@@ -195,7 +226,7 @@ function removeTaskFromLocalStorage(taskItem) {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-// Clear All Tasks
+// CLEAR all tasks from UI(function) and Local Storage(function & call)
 function clearTasks() {
     // taskList.innerHTML = '';
 
@@ -215,14 +246,14 @@ function clearTasks() {
     // https://jsperf.com/innerhtml-vs-removechild
 }
 
-// Filter Tasks
+// FILTER tasks(function)
 function filterTasks(e) {
     const text = e.target.value.toLowerCase();
 
     document.querySelectorAll('.collection-item').forEach(function(task) {
-        const item = task.firstChild.textContent;
+        const item = task.firstChild.nextElementSibling.textContent;
         if (item.toLowerCase().indexOf(text) != -1) {
-            task.style.display = 'block';
+            task.style.display = 'grid';
         } else {
             task.style.display = 'none';
         }
@@ -234,54 +265,6 @@ function filterTasks(e) {
 
 
 /*
-when item is checked add the attribute [checked="checked"]
-if (document.querySelector('.completed').checked) {
-    alert('checked - checked');
-} else {
-    alert('NOT CHECKED');
-}
-
-
-
-const myList = document.getElementById("myList");
-
-myList.addEventListener("click", function (e) {
-    const target = e.target;
-
-    if(target.matches("li")) {
-        target.style.backgroundColor = "red";
-    }
-})
-
-
-if (e.target.parentElement.classList.contains('delete-item')) {
-        if (confirm('Are you sure you want to delete this task?')) {
-            e.target.parentElement.parentElement.remove();
-
-            // Remove from Local Storage
-            removeTaskFromLocalStorage(e.target.parentElement.parentElement.firstElementChild.firstElementChild.nextElementSibling);
-        }
-    } else if (e.target.parentElement.classList.contains('edit-item')) {
-        e.target.parentElement.previousElementSibling.lastElementChild.contentEditable = true;
-        // change icons to "save" and "cancel"
-    } else if (e.target.classList.contains('completed') && e.target.checked === true ) {
-        // Add checked to item in Local Storage
-    } else if (e.target.classList.contains('save') {
-        // replace old text with new text in UI and in Local Storage
-    } else if (e.target.classList.contains('cancel') {
-        // keep old text in UI and in Local Storage
-    }
-
-
-function taskItemClicked (e) {
-    const target = e.target;
-    delete --- edit --- completed
-
-}
-
-
-
-
 
 
 
